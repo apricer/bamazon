1e0c830b2e6974e3ee5fa1cd26253cd0940d63cc
Anthony Pricer <anthony.pricer@outlook.com>
2020-02-08 09:56:56 -0600

    bamazon

--- schema.sql
+++ schema.sql
@@ -1,25 +1,35 @@
-create database bamazon;
-use bamazon;
+CREATE DATABASE bamazon;
+USE bamazon;
 
-create table products(
-Itemid integer auto_increment not null,
-ProductName varchar(45) not null,
-DepartmentName varchar(45) not null,
-Price decimal(10,4) not null,
-StockQuantity integer(10) not null,
-primary key (Itemid)
+CREATE TABLE products
+(
+    Itemid INTEGER
+    AUTO_INCREMENT NOT NULL
+ProductName VARCHAR
+    (45) NOT NULL,
+DepartmentName VARCHAR
+    (45) NOT NULL,
+Price DECIMAL
+    (10,4) NOT NULL,
+StockQuantity INTEGER
+    (10) NOT NULL,
+primary key
+    (Itemid)
 );
 
-SELECT * FROM products;
+    SELECT *
+    FROM products;
 
-INSERT INTO products (ProductName, DepartmentName, Price, StockQuantity)
-VALUES ("Rolex Datejust", "Watches", 7500.00, 1),
-  ("Rolex Submariner", "Watches", 8900.00, 3),
-  ("Rolex GMT Master II", "Watches", 9550.00, 5),
-  ("Rolex Explorer II", "Watches", 6550.00, 4),
-  ("Rolex Oyster Perpetual", "Watches", 5400.00, 11),
-  ("Rolex Yachtmaster", "Watches", 13000.00, 6),
-  ("Rolex Milgauss", "Watches", 9200.00, 3),
-  ("Cartier Tank", "Watches", 25.50, 57),
-  ("Tudor Black Bay", "Watches", 3950.00, 8),
-  ("Tudor North Flag", "Watches", 4100.00, 5);
+    INSERT INTO products
+        (ProductName, DepartmentName, Price, StockQuantity)
+    VALUES
+        ("Rolex Datejust", "Watches", 7500.00, 1),
+        ("Rolex Submariner", "Watches", 8900.00, 3),
+        ("Rolex GMT Master II", "Watches", 9550.00, 5),
+        ("Rolex Explorer II", "Watches", 6550.00, 4),
+        ("Rolex Oyster Perpetual", "Watches", 5400.00, 11),
+        ("Rolex Yachtmaster", "Watches", 13000.00, 6),
+        ("Rolex Milgauss", "Watches", 9200.00, 3),
+        ("Cartier Tank", "Watches", 25.50, 57),
+        ("Tudor Black Bay", "Watches", 3950.00, 8),
+        ("Tudor North Flag", "Watches", 4100.00, 5);
