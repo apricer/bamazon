982aa609af6cce5f7951afe86cda3dfd35aa9855
Anthony Pricer <anthony.pricer@outlook.com>
2020-02-15 12:33:51 -0600

    bamazon

--- bamazon.js
+++ bamazon.js
@@ -35,41 +35,39 @@ var promptCustomer = function (res) {
         if (answer.choice.toUpperCase() == "Q") {
             process.exit();
         }
-        for (var = 0; i < res.length; i++) {
-        if (res[i].productname == answer.choice) {
-            correct = true;
-            var product = answer.choice;
-            var id = i;
-            inquirer.prompt({
-                type: 'input',
-                name: 'quant',
-                message: "How many would you like to buy?",
-                validate: function (value) {
-                    if (isNaN(value) == false) {
-                        return true;
-                    } else {
-                        return false;
+        for (var i = 0; i < res.length; i++) {
+            if (res[i].productname == answer.choice) {
+                correct = true;
+                var product = answer.choice;
+                var id = i;
+                inquirer.prompt({
+                    type: "input",
+                    name: "quant",
+                    message: "How many would you like to buy?",
+                    validate: function (value) {
+                        if (isNaN(value) == false) {
+                            return true;
+                        } else {
+                            return false;
+                        }
                     }
-                }
-            }).then(function (answer) {
-                if ((res[id].stockquantity - answer.quant) > 0) {
-                    connection.query("UPDATE products SET
-                            stockquantity = '" + res[id].stockquantity -
-                            answer.quant) + "' WHERE productname = '" + product
-                        + "'", function (err, res2) {
-                            console.log("Product Bought!");
-                            makeTable();
-                        })
-        } else {
+                }).then(function (answer) {
+                    if ((res[id].stockquantity - answer.quant) > 0) {
+                        connection.query("UPDATE products SET stockquantity = '" + (res[id].stockquantity - answer.quant)
+                            + "' WHERE productname = '" + product + "'", function (err, res2) {
+                                console.log("Product Bought!");
+                                makeTable();
+                            })
+                    } else {
+                        console.log("Not a valid selection!");
+                        promptCustomer(res);
+                    }
+                })
+            }
+        }
+        if (i == res.length && correct == false) {
             console.log("Not a valid selection!");
             promptCustomer(res);
         }
-    })
-}
-        }
-if (i == res.length && correct == false) {
-    console.log("Not a valid selection!");
-    promptCustomer(res);
-}
     })
 }
--- schema.sql
+++ schema.sql
@@ -3,36 +3,26 @@ CREATE DATABASE bamazon;
 
 USE bamazon;
 
-CREATE TABLE products
-(
-    Itemid INTEGER
-    AUTO_INCREMENT NOT NULL,
-    ProductName VARCHAR
-    (45) NOT NULL,
-    DepartmentName VARCHAR
-    (45) NOT NULL,
-    Price DECIMAL
-    (10,2) NOT NULL,
-    StockQuantity INTEGER
-    (10) NOT NULL,
-    primary key
-    (Itemid)
+CREATE TABLE products(
+    Itemid INTEGER AUTO_INCREMENT NOT NULL,
+    ProductName VARCHAR (45) NOT NULL,
+    DepartmentName VARCHAR (45) NOT NULL,
+    Price DECIMAL (10,2) NOT NULL,
+    StockQuantity INTEGER (10) NOT NULL,
+    primary key (Itemid)
 );
 
-    SELECT *
-    FROM products;
+SELECT * FROM products;
 
-    INSERT INTO products
-        (ProductName, DepartmentName, Price, StockQuantity)
-    VALUES
-        ("Rolex Datejust", "Watches", 7500.00, 1),
-        ("Rolex Submariner", "Watches", 8900.00, 3),
-        ("Rolex GMT Master II", "Watches", 9550.00, 5),
-        ("Rolex Explorer II", "Watches", 6550.00, 4),
-        ("Rolex Oyster Perpetual", "Watches", 5400.00, 11),
-        ("Rolex Yachtmaster", "Watches", 13000.00, 6),
-        ("Rolex Milgauss", "Watches", 9200.00, 3),
-        ("Cartier Tank", "Watches", 25.50, 57),
-        ("Tudor Black Bay", "Watches", 3950.00, 8),
-        ("Tudor North Flag", "Watches", 4100.00, 5);
+INSERT INTO products (ProductName, DepartmentName, Price, StockQuantity)
+VALUES ("Rolex Datejust", "Watches", 7500.00, 1),
+    ("Rolex Submariner", "Watches", 8900.00, 3),
+    ("Rolex GMT Master II", "Watches", 9550.00, 5),
+    ("Rolex Explorer II", "Watches", 6550.00, 4),
+    ("Rolex Oyster Perpetual", "Watches", 5400.00, 11),
+    ("Rolex Yachtmaster", "Watches", 13000.00, 6),
+    ("Rolex Milgauss", "Watches", 9200.00, 3),
+    ("Cartier Tank", "Watches", 25.50, 57),
+    ("Tudor Black Bay", "Watches", 3950.00, 8),
+    ("Tudor North Flag", "Watches", 4100.00, 5);
         
